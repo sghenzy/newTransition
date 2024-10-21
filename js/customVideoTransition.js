54e2cdd34c56c15ccf30c49b00fc8e6484e2c18a
sghenzy <benedetta.bigliardo@gmail.com>
2024-10-21 15:43:42 +0200

    going back

--- js/customVideoTransition.js
+++ js/customVideoTransition.js
@@ -119,14 +119,16 @@ class VideoTransition {
     setupTweakpane() {
         const pane = new Tweakpane();
         const folder = pane.addFolder({ title: "Video Transition Settings" });
-
+    
+        // Slider per regolare l'intensità della distorsione durante la transizione
         folder.addInput(this.material.uniforms.uDistortion, "value", {
             min: 0,
             max: 0.3,
             step: 0.01,
             label: "Distortion"
         });
-
+    
+        // Slider per controllare manualmente il progresso della transizione
         folder.addInput(this.material.uniforms.uProgress, "value", {
             min: 0,
             max: 1,
@@ -134,6 +136,7 @@ class VideoTransition {
             label: "Progress"
         });
     }
+    
 }
 
 new VideoTransition('slider');
